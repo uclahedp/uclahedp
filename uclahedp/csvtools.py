28ac6551d076b842badd10480a1f79a6833649e3
Peter Heuer <pvheuer@gmail.com>
2018-11-28 13:31:08 -0800

    Documented csvtools functions better

--- uclahedp/csvtools.py
+++ uclahedp/csvtools.py
@@ -13,7 +13,22 @@ from collections import defaultdict
 
 
 def opencsv(fname):
-    d = defaultdict(lambda: [])
+    """ Opens a CSV files as a dictionary
+    Keys are taken from the first row of the csv. Columns without a key are ignored
+    Skips the 2nd and 3rd lines of the CSV, which are human-readable titles
+
+    Parameters
+    ----------
+        fname : str
+            Filepath of the CSV file
+
+    Returns
+    -------
+        csvdict: dict
+            dictionary of key:list pairs, where each key is a header from
+            row 1 and each list contains entries from rows [4,-1]
+    """
+    csvdict = defaultdict(lambda: [])
     with open(fname, 'r') as csvfile:
         reader = csv.DictReader(csvfile)
         keys = reader.fieldnames
@@ -22,12 +37,29 @@ def opencsv(fname):
         next(reader)
         for row in reader:
             for key in keys:
-                d[key].append(row[key])
-    return d
+                csvdict[key].append(row[key])
+    return csvdict
 
 
 def findvalue(csvdict, key, run=0, probe=None):
+    """ Picks out a value from a csv file dictionary
+    Returns a list of all values which match the given key, run, and probe.
+    
+    Parameters
+    ----------
+        csvdict: dict
+            Dictonary object to search
+        key: str
+            Key for the value you are searching for
+        run: int
+            Integer run number for the value you are searching for
+        probe: str
+            Name of the probe you are searching for.
 
+    Returns
+    -------
+        value: list
+    """
     if probe == None:
         value = [v for i,v in enumerate(csvdict[key]) if 
                  csvdict['run'][i] == str(run)]
@@ -38,8 +70,12 @@ def findvalue(csvdict, key, run=0, probe=None):
     return value
 
 
+
+
+
+
 if __name__ == "__main__":
     fname = r"/Volumes/PVH_DATA/LAPD_Mar2018/METADATA/CSV/bdot_runs_LAPD_Mar2018.csv"
 
     csvdict = opencsv(fname)
-    print(findvalue(csvdict, 'probe_xpos'))
+    print(findvalue(csvdict, 'probe_xpos', run=40, probe = 'LAPD1'))

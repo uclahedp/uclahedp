39385ffd241861c6c18c03cd10a02c554fe1e8c5
Peter Heuer <pvheuer@gmail.com>
2018-11-29 13:28:36 -0800

    Different attempt at class

--- uclahedp/ndfClass.py
+++ uclahedp/ndfClass.py
@@ -8,78 +8,52 @@ Created on Wed Nov 28 18:05:25 2018
 import numpy as np
 import h5py
 
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 29 13:08:18 2018
+
+@author: peter
+
+parent: ndf 
+
+
+"""
+
 class ndf:
-    
-    def __init__(self, f, filetype=None):
-            
-            self.data_unit = f['data'].attrs['unit']
-            self.pos_unit = f['pos'].attrs['unit']
-            self.t0_unit = f['t0'].attrs['unit']
-            
-            self.chan_labels = f.attrs['chan_labels']
-            self.daq = f.attrs['daq']
-            self.drive = f.attrs['drive']
-            self.plane = f.attrs['plane']
+    """Parent class for a generic HEDP dataset of any dimensionality"""
+    def __init__(self, filename): # Initialize object
+        self.filename = filename
+        self.f = h5py.File(filename, 'r')
         
+        # Init quantities that every HDF file should have
+        self.drive = self.f.attrs['drive']
 
-                
-    def open_hedp_hdf(self, f):
-        with h5py.File(rawfilepath, 'r') as f:
-            
-            self.gridded = f.attrs['gridded']
-            self.data_form = f.attrs['data_form']
-            
-            self.data = f['data'][:]
-            self.t0 = f['t0'][:]
-            
-            if self.gridded:
-                self.xgv = f['grid_xgv'] # Change to just xgv in hdf?
-                self.ygv = f['grid_ygv']
-                self.zgv = f['grid_zgv']
-                
-            if self.data_form == 'pos':
-                self.pos = f['pos'][:]
-            
-            
-            self.data_unit = f['data'].attrs['unit']
-            self.pos_unit = f['pos'].attrs['unit']
-            self.t0_unit = f['t0'].attrs['unit']
-            
-            self.chan_labels = f.attrs['chan_labels']
-            self.daq = f.attrs['daq']
-            self.drive = f.attrs['drive']
-            self.plane = f.attrs['plane']
-            
-            self.attrs = {}
-            keys = list(f.attrs)
-            for key in keys:
-                self.attrs[key] = f.attrs[key]
-        
-                
-   def open_hedp_idlsav(self,f):
-       print("Handle the idl->python conversion here")
-       
-
-   def open_lapd_hdf(self,f):
-       print("Hypothetical future function for loading the LAPD output files")
-                
-                
-                
     
-    def save(self, filepath):
-        with h5py.File(filepath, 'w') as f:
-            print(1)
-            
-
-            
-            
-
+class ndfarr (ndf):
+    """
+    Class of gridded NDF datasets
+    Inherits ndf
+    """
+    def __init__(self, filename):
+        ndf.__init__(self, filename)
+        self.dim_names = self.f.attrs['pos_labels']
+    
+    
+    
         
         
 if __name__ == "__main__":
     fname = r"/Volumes/PVH_DATA/LAPD_Mar2018/RAW/run56_LAPD1_full.h5"
+    #fname = r"/Volumes/PVH_DATA/LAPD_Mar2018/RAW/run102_PL11B_full.h5"
     
-    obj = ndf(fname)
-    
-    print(type(obj.data))
-    print(np.shape(obj.data))
+    obj = ndfarr(fname)
+    print(type(obj))
+    print(obj.dim_names)
+    print(obj.drive)
+
+
+        #    self.attrs = {}
+         #   keys = list(f.attrs)
+          #  for key in keys:
+           #     self.attrs[key] = f.attrs[key]

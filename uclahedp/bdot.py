4f66a2596e0dee81a656894d88187c4d9fa268e5
Peter Heuer <pvheuer@gmail.com>
2019-02-11 15:14:24 -0800

    Bugfix

--- uclahedp/bdot.py
+++ uclahedp/bdot.py
@@ -297,7 +297,6 @@ def bdotRawToFull(src, dest, tdiode_hdf=None, grid=False, verbose=False):
             
             del(bx,by,bz)
             del(xpol,ypol,zpol)
-
             if verbose:
                 print("End of BDOT routine!")
                 

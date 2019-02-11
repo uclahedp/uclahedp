711c62aa4de5e8e1d9b0265002055baf37278e26
Derek Schaeffer <dereks@princeton.edu>
2019-02-11 18:13:05 -0500

    Revert "Merge branch 'master' of https://github.com/phyzicist/uclahedp"
    This reverts commit a15a569772ac2c32fec77204bac89250248c917f, reversing
    changes made to f3e79be6865c869958f88f48de3dc88427566d07.

--- uclahedp/bdot.py
+++ uclahedp/bdot.py
@@ -296,7 +296,7 @@ def bdotRawToFull(src, dest, tdiode_hdf=None, grid=False, verbose=False):
             
             
             del(bx,by,bz)
-            del(xpol,ypol,zpol)
+            del(xcal,ycal,zcal,xpol,ypol,zpol)
 
             if verbose:
                 print("End of BDOT routine!")

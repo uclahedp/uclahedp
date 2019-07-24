6ee8ca4f1a3d0f2cb0ea9c002b62a68e442b94db
Peter Heuer <pvheuer@gmail.com>
2019-07-23 17:14:44 -0700

    Update LAPDscan.py

--- uclahedp/user/feister/LAPDscan.py
+++ uclahedp/user/feister/LAPDscan.py
@@ -15,18 +15,25 @@ from hrrseq import TSequence, TDevice
 
 if __name__ == "__main__":
     # Initialize sequence
-    nsteps = 401
-    nreps = 3
+    
+    ds = 5
+    dstart = 100
+    dstop = 720
+  
+    nreps = 6
     comment = "Coarse Scan of LAPD Lens, Peter's July 2019 Experiment"
     comment += "\nAuthor: Scott Feister"
-    tseq = TSequence(nsteps, nreps=nreps, comment=comment)
+    tseq = TSequence(npos, nreps=nreps, comment=comment)
 
-    # Define devices and step values
-    xmin = 360
-    xmax = 740
 
     lens = TDevice(145, unit="mm", channel=0)    
-    lens.vals = np.linspace(260, 660, tseq.nsteps) # Sequence of values
+    #lens.vals = np.linspace(260, 660, tseq.nsteps) # Sequence of values
+    lens.vals = np.arange(dstart, dstop+ds, ds)
+    
+    
+    npos = lens.vals.size
+    print("Positions: " + str(npos))
+    print("Minutes: " + str(npos*nreps/60))
         
     # Safety check for LAPD lens limits of 0 mm, 740 mm
     if np.max(lens.vals) > 740 or np.min(lens.vals) < 0:
@@ -36,10 +43,10 @@ if __name__ == "__main__":
     tseq.tdevs = [lens]
     
     # Write the sequence to file
-    outdir = r"LAPD_Outputs"
+    outdir = r"PVH_LAPD_Outputs"
     datestr = datetime.datetime.now().strftime("%Y_%m_%d")
-    outname = datestr + "_LAPD_Lens_" + str(xmin) + "_to_" + str(xmax) \
-                + "_in_" + str(nsteps) + "_steps_of_" + str(nreps) + "_reps"
+    outname = datestr + "_LAPD_Lens_" + str(dstart) + "_to_" + str(dstop) \
+                + "_in_" + str(npos) + "_steps_of_" + str(nreps) + "_reps.txt"
     outpath = os.path.join(outdir, outname)
     tseq.write(outpath)
     

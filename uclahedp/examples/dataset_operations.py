7a58f3b3c957a26ff88425c2687e4cc3e6baac3a
Peter Heuer <pvheuer@gmail.com>
2019-08-27 16:56:53 -0700

    Update dataset_operations.py

--- uclahedp/examples/dataset_operations.py
+++ uclahedp/examples/dataset_operations.py
@@ -15,20 +15,25 @@ from uclahedp.tools import dataset as dataset
 from uclahedp.process import bdot as bdot
 
 
+data_dir = os.path.join("/Volumes","PVH_DATA","LAPD_Jul2019")
+
+run = 34
+
+
 #probe = 'LAPD3'
 probe = 'LAPD_C2'
 
-src = hdftools.hdfPath(os.path.join("/Volumes","PVH_DATA","LAPD_Jul2019","FULL", "run34_LAPD3_full.hdf5"))
-trimmed = hdftools.hdfPath(os.path.join("/Volumes","PVH_DATA","LAPD_Jul2019","FULL", "trimmed.hdf5"))
-avgreps = hdftools.hdfPath(os.path.join("/Volumes","PVH_DATA","LAPD_Jul2019","FULL", "avgreps.hdf5"))
-thinned = hdftools.hdfPath(os.path.join("/Volumes","PVH_DATA","LAPD_Jul2019","FULL", "run34_" + probe + "_full_thinned.hdf5"))
-current = hdftools.hdfPath(os.path.join("/Volumes","PVH_DATA","LAPD_Jul2019","FULL", "run34_" + probe + "_full_current.hdf5"))
+src = hdftools.hdfPath(os.path.join(data_dir,"FULL", "run"+ str(run) + "_" + probe + "_full.hdf5"))
+trimmed = hdftools.hdfPath(os.path.join(data_dir,"FULL", "trimmed.hdf5"))
+avgreps = hdftools.hdfPath(os.path.join(data_dir,"FULL", "avgreps.hdf5"))
+thinned = hdftools.hdfPath(os.path.join(data_dir,"FULL", "run"+ str(run) + "_" + probe + "_full_thinned.hdf5"))
+current = hdftools.hdfPath(os.path.join(data_dir,"FULL", "run"+ str(run) + "_" + probe + "_full_current.hdf5"))
 
 print("Trimming time dimension")
 dataset.trimDim(src, trimmed, 'time', val_bounds=[0,50e-6], verbose=True)
 print("Averaging over reps dimension")
-dataset.avgDim(trimmed, avgreps, 'reps', verbose=True, delsrc=True)
+dataset.avgDim(trimmed, avgreps, 'reps', verbose=True, delsrc=False)
 print("Thinning time dimension")
-dataset.thinBin(avgreps, thinned, 'time', bin=30, verbose=True, delsrc=True)
+dataset.thinBin(avgreps, thinned, 'time', bin=30, verbose=True, delsrc=False)
 print("Generate current save file from B")
 bdot.fullToCurrent(thinned, current, verbose=False)
